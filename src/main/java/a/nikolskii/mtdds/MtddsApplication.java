725e425359b50b5d67527d1f4f7ee306d257a5e8
a.nikolskii <agnikolskii@vtb.ru>
2022-01-11 14:15:00 +0400

    init sprig boot project

---
+++ src/main/java/a/nikolskii/mtdds/MtddsApplication.java
@@ -0,0 +1,13 @@
+package a.nikolskii.mtdds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MtddsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MtddsApplication.class, args);
+	}
+
+}
