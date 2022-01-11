58d0eba3e3e8dda0e48688345082be196b0f7d2e
a.nikolskii <anikolskii@dev.vtb.ru>
2022-01-11 12:12:00 +0300

    registration controller POST

--- src/main/java/a/nikolskii/mtdds/controllers/RegistrationController.java
+++ src/main/java/a/nikolskii/mtdds/controllers/RegistrationController.java
@@ -6,8 +6,11 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.logging.Logger;
 
 
@@ -23,15 +26,24 @@ public class RegistrationController {
     @Autowired
     private final UserService userService;
 
-//    @PostMapping("/registration")
-//    public void registerUser(@RequestBody User newUser){
-//        var isCreating = userService.saveUser(newUser);
-//    }
+    @PostMapping("/registration")
+    public String registerUser(@Valid User newUser, BindingResult result, Model model){
+        log.info("POST -> /registration");
+        log.info(newUser.toString());
+        var isSaved = userService.saveUser(newUser);
+
+        if (!isSaved) {
+            var error = new ObjectError("globalError", "User already exist. Input another name.");
+            result.addError(error);
+            return "/registration";
+        }
+        return "index";
+    }
 
     @GetMapping("/registration")
     public String registration(Model model){
        log.info("GET -> /registration");
-        model.addAttribute("userForm", new User());
+        model.addAttribute("user", new User());
         return "registration";
     }
 }
